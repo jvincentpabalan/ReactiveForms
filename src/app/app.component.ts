fb77a96a47662c9a2adf12a324ffab89f892771e
JV Pabalan <jvv.pababalan@gmail.com>
2019-12-14 10:17:45 +0800

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'ReactiveForms';
+export class AppComponent implements OnInit {
+  title = "ReactiveForms";
+  sampleForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.sampleForm = this.fb.group({
+      name: this.fb.control(null, [Validators.required])
+    });
+  }
+
+  onSubmit() {
+    console.log(this.sampleForm.value);
+  }
 }
